8076c3b85cdf16d447e437e983efde725f4a38ef
FranciscoLabanca <labancafrancisco@gmail.com>
2022-06-28 20:55:35 -0300

    Agregar archivos de proyecto.

---
+++ Ahorcado.Entities/Ahorcado.cs
@@ -0,0 +1,29 @@
+﻿namespace Ahorcado.Entities
+{
+    public class Ahorcado
+    {
+        PartidaAhorcado PartidaActual;
+        List<PartidaAhorcado> _Partidas;
+
+        public Ahorcado()
+        {
+            _Partidas = new List<PartidaAhorcado>();
+        }
+
+        public void Inicializar()
+        {
+            PartidaActual = new PartidaAhorcado("Casa");
+        }
+
+        public bool IntentarLetra(string letra)
+        {
+            switch (PartidaActual.Estado)
+            {
+                case PartidaAhorcado.Estados.Jugando:
+                    return PartidaActual.RealizarIntento(letra);
+                default:
+                    return false;
+            }
+        }
+    }
+}
